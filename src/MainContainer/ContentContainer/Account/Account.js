367ac3ce8f8bd3d13e46788aa2667fa10332ca6d
bergurijohansen <bergurij@gmail.com>
2019-05-02 12:17:44 +0100

    first step

---
+++ src/MainContainer/ContentContainer/Account/Account.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+import GetAccount from './getAccounts'
+
+const Account = () => (
+  <Fragment>
+    <h1>HEY !</h1>
+    <GetAccount />
+  </Fragment>
+)
+
+export default Account
