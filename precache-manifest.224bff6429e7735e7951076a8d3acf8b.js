641c84860c52de0648a5422cfb35e0a4dcd61ee3
Olavur Ellefsen <oe@tokni.com>
2019-05-03 22:34:30 +0100

    Updates

---
+++ precache-manifest.224bff6429e7735e7951076a8d3acf8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4e756cf7790ed491b41f7c0f59d99f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a913cc32ef3ce8d38aed",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "9c18a34f8aac9aa98d9c",
+    "url": "/static/js/2.73c62e29.chunk.js"
+  },
+  {
+    "revision": "a913cc32ef3ce8d38aed",
+    "url": "/static/js/main.952f8f2d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cfo/precache-manifest.9f497383d254965814facd0a4bd87217.js"
+  "/precache-manifest.224bff6429e7735e7951076a8d3acf8b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cfo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
