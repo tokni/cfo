f0d8f06107901063e53324a45439d411b0cfe6ef
Olavur Ellefsen <oe@tokni.com>
2019-05-03 10:37:19 +0100

    Updates

---
+++ precache-manifest.6ede1292429e040f781e2c13b37958c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314a95edd83ed9b4cd12de163c3d9aee",
+    "url": "/cfo/index.html"
+  },
+  {
+    "revision": "974d605e7aa5c96ac9dc",
+    "url": "/cfo/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/cfo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "974d605e7aa5c96ac9dc",
+    "url": "/cfo/static/js/main.ef8dc71f.chunk.js"
+  },
+  {
+    "revision": "642ada9e4761642257cc",
+    "url": "/cfo/static/js/runtime~main.8ada68f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cfo/static/media/logo.5d5d9eef.svg"
+  }
+]);
