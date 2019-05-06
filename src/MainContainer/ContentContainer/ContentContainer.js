e0499d857fd4934658990e8e67b67abdf4b3a5f6
krstein2792 <kristmunds@hotmail.com>
2019-05-06 12:58:49 +0100

    removed unused line in contentContainer

--- src/MainContainer/ContentContainer/ContentContainer.js
+++ src/MainContainer/ContentContainer/ContentContainer.js
@@ -3,14 +3,11 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './Home/home'
 import Callback from '../../Callback'
 import Account from './Account/Account'
-import CreateCompany from './Company/createCompany'
-import selectCompany from './Company/selectCompany';
 
 export const ContentContainer = () => (
   <Switch>
     <Route path={`${process.env.PUBLIC_URL}/overview`} component={Home} />
     <Route path={`${process.env.PUBLIC_URL}/callback`} component={Callback} />
     <Route path={`${process.env.PUBLIC_URL}/accounts`} component={Account} />
-    <Route path={`${process.env.PUBLIC_URL}/addcompany`} component={CreateCompany} />
   </Switch>
 )
