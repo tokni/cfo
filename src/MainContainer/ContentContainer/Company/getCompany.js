b11b3c89aaa0caf5113f0bd808b871c75062bd4e
krstein2792 <kristmunds@hotmail.com>
2019-05-07 10:09:44 +0100

    added companies option to drawer

--- src/MainContainer/ContentContainer/Company/getCompany.js
+++ src/MainContainer/ContentContainer/Company/getCompany.js
@@ -1,37 +1,56 @@
-import { useSubscription } from 'react-apollo-hooks'
-import { GET_SUBSCRIP_COMPANY } from '../../../utils/query'
-import React from 'react'
+import React, { useContext } from 'react'
+import Context from '../../../Context/Context'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
 
-const Company = () => {
-  const { data, error, loading } = useSubscription(GET_SUBSCRIP_COMPANY, {
-    suspend: false,
-  })
 
-  if (loading) {
-    console.log('Loading...')
-    return (
-      <tr>
-        <td>-</td>
-      </tr>
-    )
-  }
-  if (error) {
-    console.log('Error slayer: ', error.message)
-    return (
-      <tr>
-        <td>-</td>
-      </tr>
-    )
-  }
-  return data.Company.map((item, key) => {
-    return (
-      <tr key={key}>
-        <th>{item.user_id}</th>
+const GetCompany = () => {
+  
+  // eslint-disable-next-line no-unused-vars
+  const [state, dispatch] = useContext(Context)
 
-        <td>{item.name}</td>
-      </tr>
-    )
-  })
+ 
+
+  return (
+<Paper>
+  <Table>
+    <TableHead>
+      <TableRow>
+        <TableCell>Id</TableCell>
+        <TableCell align="right">Mother Id</TableCell>
+        <TableCell align="right">User Id</TableCell>
+        <TableCell align="right">Name</TableCell>
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {state.companies
+        ? state.companies.map((item, index) => {
+            return (
+              <TableRow
+                key={index}
+              >
+                <TableCell component="th" scope="row">
+                  {item.id}
+                </TableCell>
+                <TableCell align="right">{item.mother_id}</TableCell>
+                <TableCell align="right">{item.user_id}</TableCell>
+                <TableCell align="right">{item.name}</TableCell>
+              </TableRow>
+            )
+          })
+        : console.log('getCompany not loading, state.companies undefined')}
+    </TableBody>
+  </Table>
+</Paper>
+
+  )
+  
+  
+  
 }
 
-export default Company
+export default GetCompany
