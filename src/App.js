90cfd94e5e6be299ecad759d4308cfa39cdcf3fa
bergurijohansen <bergurij@gmail.com>
2019-05-07 19:00:20 +0100

    unused import removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment } from 'react'
 import './App.css'
 import { MainContainer } from './MainContainer/MainContainer'
 import { ApolloProvider } from 'react-apollo-hooks'
