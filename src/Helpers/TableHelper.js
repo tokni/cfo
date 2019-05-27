e303b31a470d680eda4f96d4cd9c7d617f0c52f1
krstein2792 <kristmunds@hotmail.com>
2019-05-27 14:21:16 +0100

    table hlper prints all tables and functions

--- src/Helpers/TableHelper.js
+++ src/Helpers/TableHelper.js
@@ -1,70 +1,159 @@
-import React, { useContext } from 'react'
+import React, { useContext, Fragment } from 'react'
+import Language from '../utils/language'
+import PropTypes from 'prop-types'
 import {
+  withStyles,
   Table,
   TableRow,
   TableCell,
   TableHead,
   TableBody,
+  Fab,
 } from '@material-ui/core'
+import { DeleteIcon } from '../Helpers/Constants'
 import Context from '../Context/Context'
+
+const styles = theme => ({
+  fab: {
+    margin: theme.spacing.unit,
+    flexGrow: 1,
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
+  },
+})
 const TableHelper = props => {
-const [state] = useContext(Context)
-  const renderTableHeader = () => {
-    const header = props
-      ? props.array
-        ? props.array[0]
-          ? Object.keys(props.array[0])
-          : null
+  const header = props // load headers for tables
+    ? props.array
+      ? props.array[0]
+        ? Object.keys(props.array[0])
         : null
       : null
+    : null
+  
+  
+  const [state] = useContext(Context)
+  const { classes } = props
 
-      
+
+  // render tableheaders
+  const renderTableHeader = () => {
     if (header !== null) {
-      return header.map((item, index) => {  
-        if(item === '__typename')return null // skip __typename colummns
-        
-        item = stringFormatter(item, '_', ' ')
-       
-        return (<TableCell key={index}>{item}</TableCell>)
+      return header.map((item, index) => {
+        if (item === '__typename') return null // skip __typename colummns
+        item = stringFormatter(item) //format the strings so that they comply with Languages
+          // Translate the items according to language preference
+        return (
+          <TableCell key={index}>
+            {Language[state.locals][item] || item}
+          </TableCell>
+        )
       })
     }
   }
 
-  const stringFormatter = (target, search, replacement) => {
-    target = target.charAt(0).toUpperCase() + target.slice(1)
-    return target.split(search).join(replacement);
+  const stringFormatter = (target) => {
+    return target.charAt(0).toLowerCase() + target.slice(1) // make first character lowercase for every item
   }
-
-
+// render the data for every table
   const renderTableData = () => {
     if (props.array !== undefined || props.array !== null) {
-      return props.array.map((item, index) => {
-        delete item['__typename'] // delete __typename properties from array
+      return props.array.map((row, index) => {
+        delete row['__typename'] // delete __typename properties from array
         return (
           <TableRow>
-            {Object.values(item).map((row, index) => {
-              
-              if (typeof row === 'object') {
-                if (row !== null) {
-                  row = row['name']
+            {Object.values(row).map((item, rowIndex) => { //  mape the data values
+              if (typeof item === 'object') { // if the row
+                if (item !== null) {
+                  item = item['name']
                 }
-              } else if (typeof row === 'boolean') {
-                row ? (row = 'Yes') : (row = 'No')
-              } else if(typeof row === 'number'){
-                row = row.toLocaleString(state.locales)
+              } else if (typeof item === 'boolean') {
+                if (header[rowIndex] === 'paid') {
+                  item ? (item = 'Yes') : (item = 'No')
+                } else if (header[rowIndex] === 'debit') {
+                  item ? (item = 'Debit') : (item = 'Credit')
+                }
+              } else if (typeof item === 'number') {
+                item = item.toLocaleString(state.locales)
               }
 
-              
-              return <TableCell key={index}>{row}</TableCell>
+              return (
+                <Fragment>
+                  {item ? (
+                    <TableCell key={index}>{item}</TableCell>
+                  ) : (
+                    <TableCell key={index}> 
+                      {row.account_numbers
+                        ? row.account_numbers.map((account, index) => {
+                            return (
+                              account.account_number +
+                              (index + 1 === row.account_numbers.length
+                                ? ''
+                                : ' | ')
+                            )
+                          })
+                        : null}
+                    </TableCell>
+                  )}
+                </Fragment>
+              )
             })}
-            {props.update}
+            {/* Only add update button if it was passed */}
+            {props.accountNumbers ? getAccountNumbers(row.id) : null}
+            {props.payBill ? payBillsRender(row) : null}
+            {props.update ? renderUpdate(row) : null}
+            {props.delete ? renderDelete(row.id) : null}
           </TableRow>
         )
       })
     }
   }
 
+  const payBillsRender = item => {
+    return (
+      <TableCell key={item.id}>
+        {React.cloneElement(props.payBill, {...item })}
+      </TableCell>
+    )
+  }
 
+  const getAccountNumbers = id => {
+    return (
+      <TableCell key={id}>
+        {React.cloneElement(props.accountNumbers, { id })}
+      </TableCell>
+    )
+  }
+
+  const deleteHandeler = id => {
+    props.delete({
+      variables: {
+        id: id,
+      },
+    })
+  }
+
+  const renderDelete = id => {
+    return (
+      <TableCell>
+        <Fab
+          color="primary"
+          aria-label="Delete"
+          className={classes.fab}
+          onClick={deleteHandeler.bind(this, id)}
+        >
+          <DeleteIcon />
+        </Fab>
+      </TableCell>
+    )
+  }
+  const renderUpdate = item => {
+    return (
+      <TableCell key={item.id}>
+        {React.cloneElement(props.update, { ...item })}
+      </TableCell>
+    )
+  }
   return (
     <Table>
       <TableHead>
@@ -75,4 +164,8 @@ const [state] = useContext(Context)
   )
 }
 
-export default TableHelper
+TableHelper.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(TableHelper)
