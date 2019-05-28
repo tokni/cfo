640231f33f5687f803907d4b4e0caadd33073411
krstein2792 <kristmunds@hotmail.com>
2019-05-28 11:58:13 +0100

    able to crud taxes

---
+++ src/MainContainer/ContentContainer/Tax/CreateTax.js
@@ -0,0 +1,113 @@
+import Context from '../../../Context/Context'
+import Language from '../../../utils/language'
+import Modal from '../../../Helpers/Modal'
+import PropTypes from 'prop-types'
+import React, { Fragment, useState, useContext } from 'react'
+import SnackBar from '../SnackBar/SnackBar'
+import { Add } from '../../../Helpers/Constants'
+import { useMutation } from 'react-apollo-hooks'
+import { POST_TAX } from '../../../utils/Query/TaxQuery'
+import { withStyles, TextField } from '@material-ui/core'
+
+const styles = theme => ({
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
+  },
+})
+
+const CreateTax = props => {
+  const [open, setOpen] = useState(false)
+  const [taxName, setTaxName] = useState('')
+  const [taxPercentage, setTaxPercentage] = useState('')
+
+  const postTaxMutation = useMutation(POST_TAX)
+  const [state] = useContext(Context)
+  const [msg, setMsg] = useState(false)
+  const [msgSuccess, setMsgSuccess] = useState(true)
+
+  const handleClose = () => {
+    setTaxName('')
+    setTaxPercentage('')
+
+    if (state.company !== null) {
+      setOpen(!open)
+    }
+    setMsg(false)
+  }
+
+  const onSubmit = async e => {
+    if (taxName !== '' && taxPercentage !== '') {
+      await postTaxMutation({
+        variables: {
+          company_id: state.company.id,
+          name: taxName,
+          tax_percentage: taxPercentage,
+        },
+      })
+      setTimeout(() => {
+        setMsgSuccess(true)
+        setMsg(true)
+      }, 1000)
+    } else {
+      setTimeout(() => {
+        setMsgSuccess(false)
+        setMsg(true)
+      }, 1000)
+    }
+    handleClose()
+  }
+
+  return (
+    <Fragment>
+      <Modal
+        Icon={Add}
+        title="addtax"
+        text="fillformtoaddtax"
+        submit={onSubmit}
+        close={handleClose}
+      >
+        {/* name FIELD */}
+
+        <TextField
+          autoFocus
+          margin="dense"
+          id="tax"
+          value={taxName || ''}
+          label={Language[state.locals].tax || ''}
+          type="text"
+          fullWidth
+          onChange={e => {setTaxName(e.target.value)}}
+        >
+        </TextField>
+        {/* percentage field */}
+        <TextField
+          autoFocus
+          margin="dense"
+          id="taxPercentage"
+          value={taxPercentage || ''}
+          label={Language[state.locals].taxpercentage || ''}
+          type="number"
+          fullWidth
+          onChange={e => {setTaxPercentage(e.target.value)}}
+        >
+        </TextField>
+      </Modal>
+      {msg === true ? (
+        msg === true && msgSuccess === true ? (
+          <SnackBar
+            message={Language[state.locals].tax + Language[state.locals].addedsuccesfully}
+            state={'success'}
+          />
+        ) : (
+          <SnackBar message={Language[state.locals].fieldsarerequired} state={'error'} />
+        )
+      ) : null}
+    </Fragment>
+  )
+}
+
+CreateTax.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(CreateTax)
--- src/utils/Query/TaxQuery.js
+++ src/utils/Query/TaxQuery.js
@@ -24,9 +24,33 @@ query ($company_id: uuid!) {
 }
 
 `
+const POST_TAX = gql`
+  mutation postTax($company_id: uuid!, $name: String!, $tax_percentage: numeric!) {
+    insert_Tax(objects: { name: $name, company_id: $company_id , tax_percentage: $tax_percentage}) {
+      affected_rows
+    }
+  }
+`
 
+const DELETE_TAX = gql`
+  mutation deleteTax($id: uuid!) {
+    delete_Tax(where: { id: { _eq: $id } }) {
+      affected_rows
+    }
+  }
+`
 
+const PUT_TAX = gql`
+  mutation putTax($company_id: uuid!, $id: uuid!, $name: String!, $tax_percentage: numeric!) {
+    update_Tax(
+      where: { company_id: { _eq: $company_id }, id: { _eq: $id } }
+      _set: { name: $name, tax_percentage: $tax_percentage }
+    ) {
+      affected_rows
+    }
+  }
+`
 
 export {
-    GET_TAX, GET_TAX_SUBSCRIPTION
+    GET_TAX, GET_TAX_SUBSCRIPTION, POST_TAX, DELETE_TAX, PUT_TAX
 }
