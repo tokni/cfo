caba0082f0b3355a88b7366a577a07b3857045d6
krstein2792 <kristmunds@hotmail.com>
2019-06-13 11:29:41 +0100

    removed unused import

--- src/MainContainer/ContentContainer/Balance/Balance.js
+++ src/MainContainer/ContentContainer/Balance/Balance.js
@@ -1,4 +1,4 @@
-import React, { useContext, Fragment, useState } from 'react'
+import React, { useContext, Fragment } from 'react'
 import Context from '../../../Context/Context'
 import Chart from '../../../utils/Chart'
 
