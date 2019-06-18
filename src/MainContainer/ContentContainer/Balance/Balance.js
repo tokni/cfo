393f0132a62f92d5d3c7df1a943bd530fed868cd
bergurijohansen <bergurij@gmail.com>
2019-06-18 13:33:39 +0100

    filter balance

--- src/MainContainer/ContentContainer/Balance/Balance.js
+++ src/MainContainer/ContentContainer/Balance/Balance.js
@@ -1,40 +1,39 @@
-import React, { useContext, Fragment } from 'react'
+import React, { useContext, Fragment, useState } from 'react'
 import Context from '../../../Context/Context'
 import Chart from '../../../utils/Chart'
-import { Typography } from '@material-ui/core'
+import { Typography, TextField } from '@material-ui/core'
 import Language from '../../../utils/language'
 
 const Balance = () => {
   const [state] = useContext(Context)
-
-  // const getTransactions = type => {
-  //   const array = []
-  //   if (state.company && state.company.Transactions) {
-  //     state.company.Transactions.forEach(item => {
-  //       if (type === 'bills') {
-  //         if (item.bill_id !== null) {
-  //           // item.payment = item.payment * -1
-  //           array.push(item)
-  //         }
-  //       } else if (type === 'invoices') {
-  //         if (item.invoice_id !== null) {
-  //           console.log('bills else', item)
-  //           array.push(item)
-  //         }
-  //       }
-  //     })
-  //   }
-  //   array.sort((a, b) => a.time_stamp < b.time_stamp)
-
-  //   return array
-  // }
+  const [dayOffset, setDayOffset] = useState('1m')
 
   const getUnpaidBills = () => {
     const array = []
     if (state.company && state.company.Bills) {
       state.company.Bills.forEach(element => {
-        if (!element.paid) {
-          // element.payment = element.payment * -1
+        let date = new Date()
+        switch (dayOffset) {
+          case '1w':
+            date.setDate(date.getDate() + 7)
+            break
+          case '2w':
+            date.setDate(date.getDate() + 14)
+            break
+          case '1m':
+            date.setMonth(date.getMonth() + 1)
+            break
+          case '2m':
+            date.setMonth(date.getMonth() + 2)
+            break
+          case '3m':
+            date.setMonth(date.getMonth() + 3)
+            break
+          default:
+            break
+        }
+
+        if (new Date(element.payment_due) < date) {
           array.push(element)
         }
       })
@@ -49,7 +48,30 @@ const Balance = () => {
     if (state.company && state.company.Invoices) {
       state.company.Invoices.forEach(element => {
         if (!element.paid) {
-          array.push(element)
+          let date = new Date()
+          switch (dayOffset) {
+            case '1w':
+              date.setDate(date.getDate() + 7)
+              break
+            case '2w':
+              date.setDate(date.getDate() + 14)
+              break
+            case '1m':
+              date.setMonth(date.getMonth() + 1)
+              break
+            case '2m':
+              date.setMonth(date.getMonth() + 2)
+              break
+            case '3m':
+              date.setMonth(date.getMonth() + 3)
+              break
+            default:
+              break
+          }
+
+          if (new Date(element.payment_due) < date) {
+            array.push(element)
+          }
         }
       })
     }
@@ -63,6 +85,25 @@ const Balance = () => {
       <Typography variant="h4" component="h1" data-cy="title">
         {Language[state.locals]['unpaidbillsandinvoices']}
       </Typography>
+      <TextField
+        autoFocus
+        select
+        margin="dense"
+        id="offset"
+        value={dayOffset || ''}
+        label={Language[state.locals].offset || 'Offset'}
+        type="text"
+        fullWidth
+        onChange={e => {
+          setDayOffset(e.target.value)
+        }}
+      >
+        <option value={'1w'}>7 {Language[state.locals].days}</option>
+        <option value={'2w'}>14 {Language[state.locals].days}</option>
+        <option value={'1m'}>1 {Language[state.locals].month}</option>
+        <option value={'2m'}>2 {Language[state.locals].months}</option>
+        <option value={'3m'}>3 {Language[state.locals].months}</option>
+      </TextField>
       <Chart
         invoices={getUnpaidInvoices()}
         bills={getUnpaidBills()}
