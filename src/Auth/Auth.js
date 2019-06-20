09f0c64e09a9fee40a9c236114b26f559d641db1
krstein2792 <kristmunds@hotmail.com>
2019-06-20 12:25:21 +0100

    Merge pull request #100 from tokni/nestedMenu
    Nested menu

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -4,6 +4,7 @@ export const isBrowser = typeof window !== 'undefined'
 class Auth {
   accessToken
   idToken
+  tokenRenewalTimeout
 
   auth = new Auth0.WebAuth({
     domain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -16,10 +17,13 @@ class Auth {
 
   login = () => {
     this.auth.authorize()
+    this.scheduleRenewal()
   }
 
   isAuthenticated = () => {
-    return localStorage.getItem('sub')
+    // return localStorage.getItem('sub')
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'))
+    return new Date().getTime() < expiresAt
   }
 
   logout = () => {
@@ -28,10 +32,9 @@ class Auth {
     localStorage.removeItem('sub')
     localStorage.removeItem('isLoggedIn')
 
-
     this.accessToken = null
     this.idToken = null
-
+    clearTimeout(this.tokenRenewalTimeout)
     this.auth.logout()
   }
 
@@ -48,6 +51,10 @@ class Auth {
   }
 
   setSession = authResult => {
+    const expiresAt = JSON.stringify(
+      authResult.expiresIn
+    )
+    localStorage.setItem('expires_at', expiresAt)
     localStorage.setItem('isLoggedIn', 'true')
     localStorage.setItem('firstLoad', 'true')
     this.accessToken = authResult.accessToken
@@ -56,19 +63,41 @@ class Auth {
     localStorage.setItem('accessToken', authResult.accessToken)
     localStorage.setItem('sub', authResult.idTokenPayload.sub)
     localStorage.setItem('idToken', authResult.idToken)
+    this.scheduleRenewal()
   }
-
 
   handleAuthentication = () => {
     this.auth.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult)
-        window.location.reload();
-
+        window.location.reload()
       } else if (err) {
         console.log('Handle Authentication error', err)
       }
     })
   }
+
+  renewToken = () => {
+    this.auth0.checkSession({}, (err, result) => {
+      if (err) {
+        console.log(err)
+      } else {
+        this.setSession(result)
+      }
+    })
+  }
+
+  scheduleRenewal = () => {
+    if (typeof window !== 'undefined') {
+      const expiresAt = JSON.parse(localStorage.getItem('expires_at'))
+      const delay = expiresAt - Date.now()
+      if (delay > 0) {
+        this.tokenRenewalTimeout = setTimeout(() => {
+          this.renewToken()
+        }, delay)
+      }
+    }
+  }
 }
+
 export default Auth
